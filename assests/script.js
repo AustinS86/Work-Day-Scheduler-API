03ea23daf02206afd8311f32d87179ec0a4bd168
Austin Stancil <117120566+AustinS86@users.noreply.github.com>
2022-12-28 19:48:14 -0500

    added new var, local stoarge functions,

--- assests/script.js
+++ assests/script.js
@@ -13,33 +13,52 @@ $(function updateTime() {
     var timeslot3pm = ("#hour-3");
     var timeslot4pm = ("#hour-4");
     var timeslot5pm = ("#hour-5");
-    var container = $(".container-lg px-5");
     var saveBtn = $(".saveBtn");
     var timeslotElArray = [
         timeslot9am,timeslot10am,timeslot11am,timeslot12pm,timeslot1pm,timeslot2pm,timeslot3pm,timeslot4pm,timeslot5pm];
-    $("#currentDay").text(today.format("dddd, MMMM D h:mm"));
-
-
-    
-    
-    var today = dayjs().format("fpp")
+    $("#currentDay").text(today.format("dddd, MMMM D YYYY h:mm"));
+        
+    var current = dayjs().format("#currentDay");
     for (let i = 0; i < timeslotElArray.length; i++){
-        timeslotElArray[i].removeClass("future past present");
+       timeslotElArray[i].removeClass("#past, #present, #future");
 
-        if(today > timeslotElArray[i].data("hour")){
+    if(current > timeslotElArray[i].attr("hour")){
             timeslotElArray[i].addClass("past");
         
-        }else if (today === timeslotElArray[i].attr("hour")){
+        }else if (current === timeslotElArray[i].attr("hour")){
             timeslotElArray[i].addClass("present");
 
         }else{
-            timeslotElArray[i].addClass("future");
+           timeslotElArray[i].addClass("future");
         
         }
     }
 
+    $("#hour-9 .decription").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .decription").val(localStorage.getItem("hour-10"));
+    $("#hour-11.decription").val(localStorage.getItem("hour-11"));
+    $("#hour-12.decription").val(localStorage.getItem("hour12"));
+    $("#hour-1.decription").val(localStorage.getItem("hour-1"));
+    $("#hour-2 .decription").val(localStorage.getItem("hour-2"));
+    $("#hour-3 .decription").val(localStorage.getItem("hour-3"));
+    $("#hour-4 .decription").val(localStorage.getItem("hour-4"));
+    $("#hour-5 .decription").val(localStorage.getItem("hour-5"));
+    $(".saveBtn").on("click", function () {
 
+        var textarea = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
+        localStorage.setItem(textarea, time);
+
+    })
+    
+    //function submit(event){
+       // event.preventdefault();
+        
+      // }
+        
+    saveBtn.on("click", submit);
+    //});
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -48,20 +67,14 @@ $(function updateTime() {
     // useful when saving the description in local storage?
     //
     // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the today hour. HINTS: How can the id
+    // block by comparing the id to the day hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
-    // today hour in 24-hour time?
+    // day hour in 24-hour time?
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
     //
-    // TODO: Add code to display the today date in the header of the page.
-   function submit(event){
-    event.preventdefault();
-    
-   }
-    
-saveBtn.on("click", submit);
-});
+    // TODO: Add code to display the day date in the header of the page.
+
