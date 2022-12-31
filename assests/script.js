c25437ae4d6a770ebb70c76551682ea4b50cd3ef
Austin Stancil <117120566+AustinS86@users.noreply.github.com>
2022-12-31 17:58:35 -0500

    moved objects around in JS,  updated some arrays

--- assests/script.js
+++ assests/script.js
@@ -11,38 +11,29 @@
 
 $(function updateTime() {
     var today = dayjs();
-    var timeslot9am = ("#hour-9");
-    var timeslot10am = ("#hour-10");
-    var timeslot11am = ("#hour-11");
-    var timeslot12pm = ("#hour-12");
-    var timeslot1pm = ("#hour-1");
-    var timeslot2pm = ("#hour-2");
-    var timeslot3pm = ("#hour-3");
-    var timeslot4pm = ("#hour-4");
-    var timeslot5pm = ("#hour-5");
+    var timeslot9am = ("#9AM");
+    var timeslot10am = ("#10AM");
+    var timeslot11am = ("#11AM");
+    var timeslot12pm = ("#12PM");
+    var timeslot1pm = ("#1PM");
+    var timeslot2pm = ("#2PM");
+    var timeslot3pm = ("#3PM");
+    var timeslot4pm = ("#4PM");
+    var timeslot5pm = ("#5PM");
     var saveBtn = document.querySelector(".fas fa-save");
     var timeSlot = [
         timeslot9am, timeslot10am, timeslot11am, timeslot12pm, timeslot1pm, timeslot2pm, timeslot3pm, timeslot4pm, timeslot5pm];
     $("#currentDay").text(today.format("dddd, MMMM D YYYY h:mm:ss"));
 
-    $("#hour-9  ").val(localStorage.getItem("hours"));
-    $("#hour-10 ").val(localStorage.getItem("hours"));
-    $("#hour-11").val(localStorage.getItem("hour"));
-    $("#hour-12").val(localStorage.getItem("hours"));
-    $("#hour-1").val(localStorage.getItem("hours"));
-    $("#hour-2 ").val(localStorage.getItem("hours"));
-    $("#hour-3 ").val(localStorage.getItem("hours"));
-    $("#hour-4 ").val(localStorage.getItem("hours"));
-    $("#hour-5 ").val(localStorage.getItem("hours"));
-    //$(".saveBtn").on("click", function () {
-    //console.log("saveBtn");
-    //saveBtn.on("click", timeslotArray );
+    timeChange();
+    setInterval(function () {
+        $("#currentDay").text(dayjs().format("dddd, MMMM D YYYY h:mm:ss"));
+    }, 1000)
+
     renderTask();
     function renderTask() {
         var textarea = localStorage.getItem(".description");
         var time = localStorage.getItem(".saveBtn");
-        //var textarea = $(this).siblings(".hours").val();
-        //var time = $(this).parent().attr("id");
         if (!textarea || !time) {
             return;
         }
@@ -50,20 +41,14 @@ $(function updateTime() {
     }
     saveBtn = addEventListener("click", function (event) {
         event.preventDefault();
-        //console.log(".fas fa-save");
-        var textarea = document.querySelector(".time-block").value;
-        var time = document.querySelector(".saveBtn").value;
-
-
-        //console.log("saveBtn");
-        //saveBtn.on("click", timeslotArray );
-
-        //console.log(textarea, time);
+        var textarea = document.getElementById(".time-block").value;
+        var time = document.getElementById(".saveBtn").value;
 
         localStorage.setItem(".time-block", textarea);
         localStorage.setItem(".saveBtn", time);
         renderTask();
     })
+
 
 
 
@@ -74,10 +59,7 @@ $(function updateTime() {
         $('.time-block').each(function () {
             console.log($(this));
             console.log($(this).attr("id").split('-')[1]);
-            var Time = parseInt($(this).attr("id").split('-')[1]);  // --> [hour, 9]
-
-            // console.log(Time);
-            //console.log(typeof Time);
+            var Time = parseInt($(this).attr("id").split('-')[1]);  // --> [, 9]
 
             if (Time < currentHour) {
                 $(this).addClass("past");
@@ -91,16 +73,23 @@ $(function updateTime() {
             }
         })
     }
-    timeChange();
-
-
-    setInterval(function () {
-        $("#currentDay").text(dayjs().format("dddd, MMMM D YYYY h:mm:ss"));
-    }, 1000)
 
 });
 
 function init() {
-    renderTask();
+    //renderTask();
+
+    init();
 }
-init();
+// $("#-9  ").val(localStorage.getItem("hours"));
+// $("#-10 ").val(localStorage.getItem("hours"));
+ //$("#-11").val(localStorage.getItem(""));
+ //$("#-12").val(localStorage.getItem("hours"));
+ //$("#-1").val(localStorage.getItem("hours"));
+ //$("#-2 ").val(localStorage.getItem("hours"));
+ //$("#-3 ").val(localStorage.getItem("hours"));
+ //$("#-4 ").val(localStorage.getItem("hours"));
+ //$("#-5 ").val(localStorage.getItem("hours"));
+ //$(".saveBtn").on("click", function () {
+ //console.log("saveBtn");
+ //saveBtn.on("click", timeslotArray );
